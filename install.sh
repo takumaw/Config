88435aedcbb6745ca4c617667972d3bf2e286ba5
Takuma Watanabe <takumaw@sfo.kuramae.ne.jp>
2026-06-10 17:48:55 +0900

    Fix for directory with whitespace

--- install.sh
+++ install.sh
@@ -11,23 +11,24 @@ set -eEu -o pipefail
 REPO_DIR=${PWD#"$HOME"/}
 
 # Move to the home directory
-cd ${HOME}
+cd "${HOME}"
 
 symlink() {
     [[ -e "$2" || -L "$2" ]] || ln -s "$1" "$2"
 }
 
 # Create symlinks
-symlink ${REPO_DIR}/Editor/.screenrc .screenrc
-symlink ${REPO_DIR}/Editor/.vimrc .vimrc
+symlink "${REPO_DIR}/Editor/.screenrc" .screenrc
+symlink "${REPO_DIR}/Editor/.vimrc" .vimrc
 
-symlink ${REPO_DIR}/Git/.gitconfig .gitconfig
-symlink ${REPO_DIR}/Git/.gitignore_global .gitignore_global
+symlink "${REPO_DIR}/Git/.gitconfig" .gitconfig
+symlink "${REPO_DIR}/Git/.gitignore_global" .gitignore_global
 
-symlink ${REPO_DIR}/Shell/.zshrc .zshrc
+symlink "${REPO_DIR}/Shell/.zshrc" .zshrc
 
-symlink ${REPO_DIR}/SSH .ssh
-chmod 700 ${REPO_DIR}/SSH
+symlink "${REPO_DIR}/SSH" .ssh
+find "${REPO_DIR}/SSH" -type d -exec chmod 700 {} +
+find "${REPO_DIR}/SSH" -type f -exec chmod 600 {} +
 
 mkdir -p .claude
-symlink ${REPO_DIR}/Claude/CLAUDE.md .claude/CLAUDE.md
+symlink "${REPO_DIR}/Claude/CLAUDE.md" .claude/CLAUDE.md
